dd3456017664379076b952a2be0c83f4490e4315
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-06-30 00:35:56 +0300

    Functions -> TriggerHandlers

---
+++ Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob/TriggerHandlers/MyFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Lykke.Job.LykkeJob.Contract;
+using Lykke.Job.LykkeJob.Core;
+using Lykke.JobTriggers.Triggers.Attributes;
+
+namespace Lykke.Job.LykkeJob.Functions
+{
+    // NOTE: This is the example trigger handlers class
+    public class MyHandlers
+    {
+        // NOTE: The object is instantiated using DI container, so registered dependencies are injected well
+        public MyHandlers(AppSettings.LykkeJobSettings appSettings)
+        {
+        }
+
+        [TimerTrigger("00:00:10")]
+        public async Task TimeTriggeredHandler()
+        {
+            await Task.FromResult(0);
+        }
+
+        [QueueTrigger("queue-name")]
+        public async Task QueueTriggeredHandler(MyMessage msg)
+        {
+            await Task.FromResult(0);
+        }
+    }
+}
