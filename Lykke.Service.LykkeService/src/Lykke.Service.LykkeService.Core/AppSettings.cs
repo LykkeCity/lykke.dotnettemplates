32775c3dce04ee9be5f8b84e98d1f3b4b4aa9296
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-08-26 13:03:46 +0300

    SlackNotifications.ThrottlingLimitSeconds was removed

--- Lykke.Service.LykkeService/src/Lykke.Service.LykkeService.Core/AppSettings.cs
+++ Lykke.Service.LykkeService/src/Lykke.Service.LykkeService.Core/AppSettings.cs
@@ -19,8 +19,6 @@
     public class SlackNotificationsSettings
     {
         public AzureQueueSettings AzureQueue { get; set; }
-
-        public int ThrottlingLimitSeconds { get; set; }
     }
 
     public class AzureQueueSettings
