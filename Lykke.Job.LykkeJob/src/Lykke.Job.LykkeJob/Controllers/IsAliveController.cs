4b7122d05f9b71c0a2d9d711fe2f04537a0d113f
Dmitry Samodov <samodov.dmitry@gmail.com>
2017-11-16 19:58:17 +0300

    make job template compile, added appsettings.Development.json files

--- Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob/Controllers/IsAliveController.cs
+++ Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob/Controllers/IsAliveController.cs
@@ -4,7 +4,7 @@ using System.Net;
 using Lykke.Job.LykkeJob.Core.Services;
 using Lykke.Job.LykkeJob.Models;
 using Microsoft.AspNetCore.Mvc;
-using Swashbuckle.SwaggerGen.Annotations;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Lykke.Job.LykkeJob.Controllers
 {
