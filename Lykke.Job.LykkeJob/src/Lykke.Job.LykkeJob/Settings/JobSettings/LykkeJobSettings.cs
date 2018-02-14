3fa978d1344b05e2478293c773eeed8a0ae5ab4d
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-02-14 16:45:35 +0300

    Fixed template for a job

--- Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob/Settings/JobSettings/LykkeJobSettings.cs
+++ Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob/Settings/JobSettings/LykkeJobSettings.cs
@@ -1,13 +1,13 @@
 ﻿namespace Lykke.Job.LykkeJob.Settings.JobSettings
 {
-        public class LykkeJobSettings
-        {
-            public DbSettings Db { get; set; }
+    public class LykkeJobSettings
+    {
+        public DbSettings Db { get; set; }
 #if azurequeuesub
-            public AzureQueueSettings AzureQueue { get; set; }
+        public AzureQueueSettings AzureQueue { get; set; }
 #endif
 #if (rabbitsub || rabbitpub)
-            public RabbitMqSettings Rabbit { get; set; }
+        public RabbitMqSettings Rabbit { get; set; }
 #endif
-        }
+    }
 }
