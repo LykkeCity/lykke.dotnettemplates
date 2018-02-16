6163f226a27ab7bbd24aa8a157dc91b9d5fb6f1d
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-02-16 15:41:32 +0300

    Added registration in StopManager for job template

--- Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob.Core/Services/IShutdownManager.cs
+++ Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob.Core/Services/IShutdownManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Threading.Tasks;
+using Common;
 
 namespace Lykke.Job.LykkeJob.Core.Services
 {
     public interface IShutdownManager
     {
         Task StopAsync();
+
+        void Register(IStopable stopable);
     }
 }
--- Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob.Services/ShutdownManager.cs
+++ Lykke.Job.LykkeJob/src/Lykke.Job.LykkeJob.Services/ShutdownManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common;
 using Common.Log;
 using Lykke.Job.LykkeJob.Core.Services;
 
@@ -12,15 +14,24 @@ namespace Lykke.Job.LykkeJob.Services
     public class ShutdownManager : IShutdownManager
     {
         private readonly ILog _log;
+        private readonly List<IStopable> _items = new List<IStopable>();
 
         public ShutdownManager(ILog log)
         {
             _log = log;
         }
 
+        public void Register(IStopable stopable)
+        {
+            _items.Add(stopable);
+        }
+
         public async Task StopAsync()
         {
-            // TODO: Implement your shutdown logic here. Good idea is to log every step
+            foreach (var item in _items)
+            {
+                item.Stop();
+            }
 
             await Task.CompletedTask;
         }
