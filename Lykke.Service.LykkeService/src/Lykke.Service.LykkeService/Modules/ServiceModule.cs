b7ed2ba64e3d544d3893da26143ace1e7837c148
Mikhail Musin <mikhail.musin@gmail.com>
2018-05-30 01:58:33 +0300

    sdk

--- Lykke.Service.LykkeService/src/Lykke.Service.LykkeService/Modules/ServiceModule.cs
+++ Lykke.Service.LykkeService/src/Lykke.Service.LykkeService/Modules/ServiceModule.cs
@@ -1,15 +1,22 @@
 ﻿using Autofac;
 using Lykke.Sdk;
 using Lykke.Service.LykkeService.Settings;
+using Lykke.SettingsReader;
 
 namespace Lykke.Service.LykkeService.Modules
 {    
     public class ServiceModule : Module
     {
+        private readonly IReloadingManager<AppSettings> _appSettings;
+
+        public ServiceModule(IReloadingManager<AppSettings> appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
         protected override void Load(ContainerBuilder builder)
         {
             // Do not register entire settings in container, pass necessary settings to services which requires them
-            // example: builder.RegisterSettings<ChaosKittySettings>("LykkeServiceService.ChaosKitty");
         }
     }
 }
