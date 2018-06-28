c142d64305bc858fa705f228412afdd348bf92c1
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-06-28 15:53:21 +0300

    Restored validation

--- Lykke.Service.LykkeService/client/Lykke.Service.LykkeService.Client/AutofacExtension.cs
+++ Lykke.Service.LykkeService/client/Lykke.Service.LykkeService.Client/AutofacExtension.cs
@@ -11,14 +11,21 @@ namespace Lykke.Service.LykkeService.Client
         /// <summary>
         /// Registers <see cref="ILykkeServiceClient"/> in Autofac container using <see cref="LykkeServiceServiceClientSettings"/>.
         /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="settings"></param>
-        /// <param name="builderConfigure"></param>
+        /// <param name="builder">Autofac container builder.</param>
+        /// <param name="settings">LykkeService client settings.</param>
+        /// <param name="builderConfigure">Optional <see cref="HttpClientGeneratorBuilder"/> configure handler.</param>
         public static void RegisterLykkeServiceClient(
-            this ContainerBuilder builder,
-            LykkeServiceServiceClientSettings settings,
-            Func<HttpClientGeneratorBuilder, HttpClientGeneratorBuilder> builderConfigure)
+            [NotNull] this ContainerBuilder builder,
+            [NotNull] LykkeServiceServiceClientSettings settings,
+            [CanBeNull] Func<HttpClientGeneratorBuilder, HttpClientGeneratorBuilder> builderConfigure)
         {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+            if (string.IsNullOrWhiteSpace(settings.ServiceUrl))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(LykkeServiceServiceClientSettings.ServiceUrl));
+
             builder.RegisterClient<ILykkeServiceClient>(settings?.ServiceUrl, builderConfigure);
         }
     }
