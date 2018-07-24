82452326c6bc3bbcb67799ed14a6c45e652ba4a2
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-07-24 14:10:59 +0300

    Made API properties publicly available for multiple APIs case

--- Lykke.Service.LykkeService/client/Lykke.Service.LykkeService.Client/LykkeServiceClient.cs
+++ Lykke.Service.LykkeService/client/Lykke.Service.LykkeService.Client/LykkeServiceClient.cs
@@ -4,18 +4,18 @@ namespace Lykke.Service.LykkeService.Client
 {
     public class LykkeServiceClient : ILykkeServiceClient
     {
-        //In case of several api controllers in your app - uncomment and use multiple APIs code below.
+        //In case of several api controllers in your app - uncomment and use multiple APIs code below instead of one private field.
         private ILykkeServiceClient _api;
 
-        //private IControllerApi1 _api1;
-        //private IControllerApi2 _api2;
+        //public IControllerApi1 Api1 { get; private set; }
+        //public IControllerApi2 Api2 { get; private set; }
 
         public LykkeServiceClient(IHttpClientGenerator httpClientGenerator)
         {
             _api = httpClientGenerator.Generate<ILykkeServiceClient>();
 
-            //_api1 = httpClientGenerator.Generate<IControllerApi1>();
-            //_api2 = httpClientGenerator.Generate<IControllerApi2>();
+            //Api1 = httpClientGenerator.Generate<IControllerApi1>();
+            //Api2 = httpClientGenerator.Generate<IControllerApi2>();
         }
     }
 }
