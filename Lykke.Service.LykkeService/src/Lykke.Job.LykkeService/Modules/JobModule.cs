5398e3d05caa06a6d49d27bfbfa14b957808c5f7
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-09-24 14:23:42 +0300

    changed project types - Service - for service only projects, ServiceJob - for both service and job projects

--- Lykke.Service.LykkeService/src/Lykke.Job.LykkeService/Modules/JobModule.cs
+++ Lykke.Service.LykkeService/src/Lykke.Job.LykkeService/Modules/JobModule.cs
@@ -21,7 +21,6 @@ using Lykke.Job.LykkeService.Contract;
 using Lykke.Job.LykkeService.RabbitPublishers;
 using Lykke.RabbitMq.Azure;
 using Lykke.RabbitMqBroker.Publisher;
-using Lykke.LykkeType.LykkeService.Domain.Services;
 #endif
 using Lykke.SettingsReader;
 using Microsoft.Extensions.DependencyInjection;
